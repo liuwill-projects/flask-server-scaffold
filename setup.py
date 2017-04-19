42e1f6d1d65ebb9ef3c9fe8e196666f04ddee863
liuwill <liuwei_will@qq.com>
2017-04-19 20:41:06 +0800

    [add] socket io support

--- setup.py
+++ setup.py
@@ -6,14 +6,17 @@ setup(
     version='1.0',
     author='liuwill',
     author_email='liuwill@live.com',
+    url='http://www.liuwill.com',
     install_requires=[
         'flask>=0.12.1',
+        'Flask-SocketIO>=2.8.6',
         'Flask-Cors>=3.0.2',
         'Jinja2>=2.9.6'
     ],
+    packages=["chat"],
     #packages=['']
     #py_modules=['foo'],
-    scripts=["server.py"],
+    scripts=["main.py"],
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
 )
